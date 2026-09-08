2d5c402f0c236076adc63217aa9255f55332472f
Pratham Patel <prathampatel@thefossguy.com>
2026-09-08 20:24:37 +0530

    systemd-rollbacker-generator: redo systemd unit generation to be more readable and _correct_

--- src/systemd-rollbacker-generator/main.rs
+++ src/systemd-rollbacker-generator/main.rs
@@ -8,21 +8,20 @@ pub const CARGO_BIN_NAME: &str = env!("CARGO_BIN_NAME");
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = std::env::args().collect::<Vec<String>>();
-    let Some(arg0) = args.get(0) else {
+    let Some(first_arg) = args.first() else {
         return Err("Could not determine the 0th argv".into());
     };
-    let Ok(canonicalized_arg0) = std::fs::canonicalize(arg0) else {
-        return Err(format!("Could not canonicalize the arg0 '{arg0}'").into());
+    let Ok(canonicalized_first_arg) = std::fs::canonicalize(first_arg) else {
+        return Err(format!("Could not canonicalize the arg0 '{first_arg}'").into());
     };
-    let Some(arg0_bin_dir) = canonicalized_arg0.parent() else {
+    let Some(first_arg_bin_dir) = canonicalized_first_arg.parent() else {
         return Err(format!(
             "Could not determine the parent of '{}'",
-            canonicalized_arg0.display()
+            canonicalized_first_arg.display()
         )
         .into());
     };
-    let arg0_bin_dir = arg0_bin_dir.display().to_string();
-    eprintln!("{arg0_bin_dir}");
+    let first_arg_bin_dir = first_arg_bin_dir.display().to_string();
 
     let systemd_rollbacker_generator_config = config::configure()?;
     eprintln!("{systemd_rollbacker_generator_config:#?}");
@@ -33,6 +32,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     systemd_generator::generate_systemd_services(
         &systemd_rollbacker_generator_config,
         &parsed_fstab,
-        &arg0_bin_dir,
+        &first_arg_bin_dir,
     )
 }
--- src/systemd-rollbacker-generator/systemd_generator.rs
+++ src/systemd-rollbacker-generator/systemd_generator.rs
@@ -8,7 +8,8 @@ use std::process::Command;
 use tfg_helpers::command_helpers::CommandOutputStatus;
 use tfg_helpers::{get_process_stderr, log_then_output, make_formatted_error};
 
-pub const SYSTEMD_GENERATOR_DIR: &str = "/run/systemd/system";
+//pub const SYSTEMD_GENERATOR_DIR: &str = "/run/systemd/system";
+pub const SYSTEMD_GENERATOR_DIR: &str = "/home/pratham/my-git-repos/pratham/staging/rollbacker";
 
 fn strip_multi_line_prefix(target_string: &str) -> String {
     target_string
@@ -42,15 +43,15 @@ fn generate_systemd_btrfs_services(
     systemd_rollbacker_generator_config: &SystemdRollbackerGeneratorConfig,
     superblock_entry: &SuperblockEntry,
     device_mount_paths: &DeviceMountPaths,
-    arg0_bin_dir: &str,
-) -> Result<(), Box<dyn Error>> {
-    let mount_units = convert_device_mount_paths_to_mount_units(device_mount_paths);
+    first_arg_bin_dir: &str,
+) -> Vec<String> {
+    let mut systemd_units_to_enable = Vec::new();
     let mount_path = superblock_entry.mount_path.clone().unwrap();
-    let superblock_mount_base_unit_name = systemd_escape(&mount_path);
-    let superblock_mount_base_unit =
-        format!("{SYSTEMD_GENERATOR_DIR}/{superblock_mount_base_unit_name}");
+    let mount_path_systemd_escaped = systemd_escape(&mount_path);
 
-    let superblock_mount_unit_path = format!("{superblock_mount_base_unit}.mount");
+    let superblock_mount_unit_name = format!("{mount_path_systemd_escaped}.mount");
+    let superblock_mount_unit_path =
+        format!("{SYSTEMD_GENERATOR_DIR}/{superblock_mount_unit_name}");
     let superblock_mount_unit_contents = strip_multi_line_prefix(
         format!(
             "
@@ -59,101 +60,117 @@ fn generate_systemd_btrfs_services(
 
         [Mount]
         What={}
-        Where={}
+        Where={mount_path}
         Type={}
         Options=subvolid=5
-            ",
+        ",
             systemd_escape(&superblock_entry.device),
             superblock_entry.device,
-            mount_path,
             superblock_entry.filesystem_type,
         )
         .as_str(),
     );
-    if let Err(e) = fs::write(&superblock_mount_unit_path, superblock_mount_unit_contents) {
-        return Err(format!(
-            "Could not write to {superblock_mount_unit_path}{}",
+    match fs::write(&superblock_mount_unit_path, &superblock_mount_unit_contents) {
+        Ok(()) => systemd_units_to_enable.push(superblock_mount_unit_name.clone()),
+        Err(e) => eprintln!(
+            "ERROR: Could not write to '{superblock_mount_unit_path}'{}",
             make_formatted_error!(e)
-        )
-        .into());
+        ),
     }
 
-    let rollback_service_unit_path = format!("{superblock_mount_base_unit}.service");
+    let systemd_mount_units = convert_device_mount_paths_to_mount_units(device_mount_paths);
+    let rollback_service_unit_name = format!("rollbacker-{mount_path_systemd_escaped}.service");
+    let rollback_service_unit_path =
+        format!("{SYSTEMD_GENERATOR_DIR}/{rollback_service_unit_name}");
     let rollback_service_unit_contents = strip_multi_line_prefix(format!("
         [Unit]
-        Requires={superblock_mount_base_unit_name}.mount
-        After={superblock_mount_base_unit_name}.mount
-        Before=sysroot.mount {mount_units}
-        RequiredBy=sysroot.mount {mount_units}
+        After={superblock_mount_unit_name}
+        Requires={superblock_mount_unit_name}
+        Before=sysroot.mount {systemd_mount_units}
 
         [Service]
         Type=oneshot
         RemainAfterExit=yes
-        ExecStart={arg0_bin_dir}/rollbacker --filesystem-type {} --fresh-snapshot-suffix {} --superblock-path {mount_path}
-        ", superblock_entry.filesystem_type, systemd_rollbacker_generator_config.fresh_snapshot_suffix).as_str());
-    if let Err(e) = fs::write(&rollback_service_unit_path, rollback_service_unit_contents) {
-        return Err(format!(
-            "Could not write to {rollback_service_unit_path}{}",
+        ExecStart={first_arg_bin_dir}/rollbacker --filesystem-type {} --fresh-snapshot-suffix {} --superblock-path {mount_path}
+
+        [Install]
+        RequiredBy={systemd_mount_units}
+        ", superblock_entry.filesystem_type, systemd_rollbacker_generator_config.fresh_snapshot_suffix)
+        .as_str());
+    match fs::write(&rollback_service_unit_path, &rollback_service_unit_contents) {
+        Ok(()) => systemd_units_to_enable.push(rollback_service_unit_name),
+        Err(e) => eprintln!(
+            "ERROR: Could not write to '{rollback_service_unit_path}'{}",
             make_formatted_error!(e)
-        )
-        .into());
+        ),
     }
 
-    Ok(())
+    systemd_units_to_enable
 }
 fn generate_systemd_zfs_services(
     systemd_rollbacker_generator_config: &SystemdRollbackerGeneratorConfig,
     superblock_entry: &SuperblockEntry,
     device_mount_paths: &DeviceMountPaths,
-    arg0_bin_dir: &str,
-) -> Result<(), Box<dyn Error>> {
-    let mount_units = convert_device_mount_paths_to_mount_units(device_mount_paths);
-    let mount_path = superblock_entry.device.clone();
-    let superblock_mount_base_unit =
-        format!("{SYSTEMD_GENERATOR_DIR}/{}", systemd_escape(&mount_path));
+    first_arg_bin_dir: &str,
+) -> String {
+    let zpool_name = superblock_entry.device.clone();
+    let systemd_mount_units = convert_device_mount_paths_to_mount_units(device_mount_paths);
 
-    let rollback_service_unit_path = format!("{superblock_mount_base_unit}.service");
-    let rollback_service_unit_contents = strip_multi_line_prefix(format!("
+    let zfs_dataset_rollback_unit_name = format!("rollbacker-{zpool_name}.service");
+    let zfs_dataset_rollback_unit_path =
+        format!("{SYSTEMD_GENERATOR_DIR}/{zfs_dataset_rollback_unit_name}");
+    let zfs_dataset_rollback_unit_contents = strip_multi_line_prefix(format!("
         [Unit]
-        Requires=zfs-import-{}.service
-        After=zfs-import-{}.service
-        RequiredBy=zfs-import.target {mount_units}
-        Before=zfs-import.target {mount_units}
+        Requires=zfs-import-{zpool_name}.service
+        After=zfs-import-{zpool_name}.service
+        Before={systemd_mount_units}
 
         [Service]
         Type=oneshot
         RemainAfterExit=yes
-        ExecStart={arg0_bin_dir}/rollbacker --filesystem-type {} --fresh-snapshot-suffix {} --superblock-path {mount_path}
-        ", superblock_entry.device, superblock_entry.device, superblock_entry.filesystem_type, systemd_rollbacker_generator_config.fresh_snapshot_suffix).as_str());
-    if let Err(e) = fs::write(&rollback_service_unit_path, rollback_service_unit_contents) {
-        return Err(format!(
-            "Could not write to {rollback_service_unit_path}{}",
-            make_formatted_error!(e)
-        )
-        .into());
+        ExecStart={first_arg_bin_dir}/rollbacker --filesystem-type {} --fresh-snapshot-suffix {} --superblock-path {zpool_name}
+
+        [Install]
+        RequiredBy={systemd_mount_units}
+        ", superblock_entry.filesystem_type, systemd_rollbacker_generator_config.fresh_snapshot_suffix)
+        .as_str());
+    match fs::write(
+        &zfs_dataset_rollback_unit_path,
+        &zfs_dataset_rollback_unit_contents,
+    ) {
+        Ok(()) => zfs_dataset_rollback_unit_name,
+        Err(e) => {
+            eprintln!(
+                "ERROR: Could not write to '{zfs_dataset_rollback_unit_path}'{}",
+                make_formatted_error!(e)
+            );
+            String::new()
+        }
     }
-    Ok(())
 }
 
 pub fn generate_systemd_services(
     systemd_rollbacker_generator_config: &SystemdRollbackerGeneratorConfig,
     parsed_fstab: &ParsedFstab,
-    arg0_bin_dir: &str,
+    first_arg_bin_dir: &str,
 ) -> Result<(), Box<dyn Error>> {
+    let mut systemd_units_to_enable = vec!["enable".to_string()];
     for (superblock_entry, device_mount_paths) in parsed_fstab {
         match superblock_entry.filesystem_type {
-            FilesystemType::Btrfs => generate_systemd_btrfs_services(
+            FilesystemType::Btrfs => {
+                systemd_units_to_enable.extend(generate_systemd_btrfs_services(
+                    systemd_rollbacker_generator_config,
+                    superblock_entry,
+                    device_mount_paths,
+                    first_arg_bin_dir,
+                ));
+            }
+            FilesystemType::Zfs => systemd_units_to_enable.push(generate_systemd_zfs_services(
                 systemd_rollbacker_generator_config,
                 superblock_entry,
                 device_mount_paths,
-                arg0_bin_dir,
-            )?,
-            FilesystemType::Zfs => generate_systemd_zfs_services(
-                systemd_rollbacker_generator_config,
-                superblock_entry,
-                device_mount_paths,
-                arg0_bin_dir,
-            )?,
+                first_arg_bin_dir,
+            )),
             _ => eprintln!(
                 "The filesystem '{}' does not support rollbacks, generating nothing",
                 superblock_entry.filesystem_type
@@ -165,7 +182,21 @@ pub fn generate_systemd_services(
     systemctl_daemon_reload_cmd.arg("daemon-reload");
     let systemctl_daemon_reload_process_result = log_then_output!(systemctl_daemon_reload_cmd);
     if (&systemctl_daemon_reload_process_result).was_process_successful() {
-        Ok(())
+        let mut systemctl_enable_cmd = Command::new("systemctl");
+        systemctl_enable_cmd
+            .arg("enable")
+            .args(systemd_units_to_enable.iter().collect::<Vec<&String>>());
+        let systemctl_enable_process_result = log_then_output!(systemctl_enable_cmd);
+        if (&systemctl_enable_process_result).was_process_successful() {
+            Ok(())
+        } else {
+            let systemctl_enable_process_output = systemctl_enable_process_result?;
+            Err(format!(
+                "The command to enable the newly generated systemd units failed{}",
+                get_process_stderr!(systemctl_enable_process_output)
+            )
+            .into())
+        }
     } else {
         let systemctl_daemon_reload_process_output = systemctl_daemon_reload_process_result?;
         Err(format!(
