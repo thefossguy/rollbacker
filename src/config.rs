f800d2c792c0e33eec5c7fc4f0ac21fe5eead75f
Pratham Patel <prathampatel@thefossguy.com>
2026-09-09 20:42:47 +0530

    license: init to `GPL-2.0-only`

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0-only
+// Copyright (C) 2026 Pratham Patel <prathampatel@thefossguy.com>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; only as version 2
+// of the License, NOT as a later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, see <https://www.gnu.org/licenses/>.
+
 use std::error::Error;
 use std::fmt;
 
